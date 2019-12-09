4894c85578ddd35b59311c9b9fcae8514d8272c0
hewei <hwjf123456@sina.cn>
2019-12-09 20:08:01 +0800

    添加收藏、趋势页面

---
+++ views/Index/Trending/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+export default class TrendingView extends React.Component {
+  render(): React.ReactNode {
+    return (
+      <View>
+        <Text>TrendingView</Text>
+      </View>
+    );
+  }
+}
--- views/Index/index.js
+++ views/Index/index.js
@@ -3,9 +3,14 @@ import { View, Text } from 'react-native';
 
 import { createAppContainer } from 'react-navigation';
 import { createBottomTabNavigator } from 'react-navigation-tabs';
+
+import MaterialIcons from 'react-native-vector-icons/MaterialIcons';
 import Ionicons from 'react-native-vector-icons/Ionicons';
+
 import HomeView from './Home';
 import MineView from './Mine';
+import TrendingView from './Trending';
+import FavoriteView from './Favorite';
 
 const primary = '#1890ff';
 
@@ -15,13 +20,43 @@ const TabBar = createAppContainer(createBottomTabNavigator({
     navigationOptions: {
       title: '首页',
       tabBarIcon: ({ focused }) => (
-        <Ionicons
-          name='md-home'
+        <MaterialIcons
+          name='whatshot'
           size={28}
           style={{ color: focused ? primary : 'gray' }}/>
       ),
       tabBarLabel: ({ focused }) => (
         <Text style={{ textAlign: 'center', color: focused ? primary : 'gray' }}>首页</Text>
+      ),
+    },
+  },
+  TrendingView:{
+    screen: TrendingView,
+    navigationOptions: {
+      title: '趋势',
+      tabBarIcon: ({ focused }) => (
+        <MaterialIcons
+          name='trending-up'
+          size={28}
+          style={{ color: focused ? primary : 'gray' }}/>
+      ),
+      tabBarLabel: ({ focused }) => (
+        <Text style={{ textAlign: 'center', color: focused ? primary : 'gray' }}>趋势</Text>
+      ),
+    },
+  },
+  FavoriteView:{
+    screen: FavoriteView,
+    navigationOptions: {
+      title: '收藏',
+      tabBarIcon: ({ focused }) => (
+        <MaterialIcons
+          name='favorite'
+          size={28}
+          style={{ color: focused ? primary : 'gray' }}/>
+      ),
+      tabBarLabel: ({ focused }) => (
+        <Text style={{ textAlign: 'center', color: focused ? primary : 'gray' }}>收藏</Text>
       ),
     },
   },
