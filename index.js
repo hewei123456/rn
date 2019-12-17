6d51521c56068aa8d67ff2df7a77b10a782957f6
hewei <hewei95324@offcn.com>
2019-12-17 08:53:03 +0800

    删除App

--- index.js
+++ index.js
@@ -1,9 +1,24 @@
-/**
- * @format
- */
+import { AppRegistry } from 'react-native';
+import { name as appName } from './app.json';
 
-import {AppRegistry} from 'react-native';
-import App from './App';
-import {name as appName} from './app.json';
+import React, { Component } from 'react';
+
+import { createAppContainer } from 'react-navigation';
+import { SwitchNavigator } from './router';
+
+import { Provider } from 'react-redux';
+import configStore from './store';
+
+const store = configStore();
+
+const Index = createAppContainer(SwitchNavigator);
+
+export class App extends Component {
+  render(): React.ReactNode {
+    return <Provider store={store}>
+      <Index/>
+    </Provider>;
+  }
+}
 
 AppRegistry.registerComponent(appName, () => App);
